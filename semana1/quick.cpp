0045d6d717c23c41ba1dc8f936d087d9a7774ad5
Erick <ericklopez.j@uni.pe>
2026-04-09 18:15:02 -0500

    Corrigiendo

--- semana1/quick.cpp
+++ semana1/quick.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
 using namespace std;
 
-void quickSort (int arr[], int  inicio, int fin) {
+void quickSort (int arr[], int inicio, int fin) {
     if (inicio < fin) {
         int pivote = arr[fin];
+
         int i = inicio - 1;
 
-        for (int j = inicio; j < fin; j++) {
+        for (int j = inicio; j < fin; j++) { //j < fin porque nuestro pivote convenientemente es el último
             if (arr[j] < pivote) {
                 i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
 
-            int temp = arr[j];
-            arr[j] = arr[i];
-            arr[i] = temp;
         }
 
-        int temp = arr[fin];
-        arr[fin] = arr[i + 1];
-        arr[i + 1] = temp;
+        int temp = arr[i + 1]; //cuidado con poner pivote en vez de arr[fin], ya que queremos intercambiar fisicamente valores del arr
+        arr[i + 1] = arr[fin];
+        arr[fin] = temp;
+
+        
 
         int indPivote = i + 1;
-
         quickSort (arr, inicio, indPivote - 1);
         quickSort (arr, indPivote + 1, fin);
     }
+
+
 }
